e03d5912e4e1620acec985d8808cf8ff12562230
GrahamP98 <g.purnell15@gmail.com>
2022-06-06 15:45:13 -0400

    erased search-results.html, added styling

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// FUNCTION TO RETRIEVE VIDEO BASED ON KEYWORD
-//function searchByKeyword() {
-//     var results = YouTube.Search.list('id,snippet', { q: 'dogs', maxResults: 25 });
-
-//     for (var i in results.items) {
-//         var item = results.items[i];
-//         Logger.log('[%s] Title: %s', item.id.videoId, item.snippet.title);
-//     }
-// }
 // https://www.googleapis.com/youtube/v3/search YOUTUBE API
 // https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyCtctob-kkfgeC-tLfL8Yz5KCWNvkjXObA&type=video&q=drake
 $(document).ready(function(){
@@ -42,56 +33,3 @@ $(document).ready(function(){
     }
 
 })
-
-// var searchResultsEl = document.querySelector('#search-results');
-// var qInput = document.querySelector('#q');
-// var formatInput = document.querySelector('#format');
-// var searchForm = document.querySelector('#artist-search');
-
-
-
-// var getSearchResults = function (q) {
-//     var searchURL = "";
-
-//     fetch(searchURL)
-//         .then(function (response) {
-//             console.log(response);
-//             return response.json();
-
-//         })
-//     .then(function (data) {
-//         searchResultsEl.innerHTML = null;
-
-//     })
-// }
-
-
-
-
-
-// var init = function () {
-//     if (location.search) {
-//         var url = new URL(location.href);
-//         var q = url.searchParams.get('q');
-//         var format = url.searchParams.get('format');
-//         getSearchResults(q, format);
-//     }
-// };
-
-// var handleSearch = function (event) {
-//     event.preventDefault();
-//     var q = qInput.value.trim();
-
-//     if (!q) return;
-
-//     if (searchResultsEl) {
-//         getSearchResults(q);
-//     } else {
-//         location.replace('./search-results.html?q=' + q);
-//     }
-// }
-
-// searchForm.addEventListener('submit', handleSearch);
-
-
-// init();
